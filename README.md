6cd45b927cde9a591d6c113f32f0e6362febadee
ssj <2740635052@qq.com>
2023-10-18 19:46:23 +0800

    push code

---
+++ README.md
@@ -0,0 +1,12 @@
+# mychatserver
+Linux下基于muduo的C++集群聊天服务器
+
+### 编译项目
+./autobuild.sh
+
+### 启动项目
+#### 启动服务器
+./bin/ChatServer 127.0.0.1 6000
+#### 启动客户端
+./bin/ChatClient 127.0.0.1 8000
+
---
+++ include/server/model/friendmodel.hpp
@@ -0,0 +1,21 @@
+#ifndef FRIENDMODEL_H
+#define FRIENDMODEL_H
+
+#include "user.hpp"
+#include <vector>
+using namespace std;
+
+
+// 提供好友信息的操作接口方法
+class FriendModel
+{
+public:
+    // 添加好友关系
+    void insert(int userid, int friendid);
+
+    // 返回用户好友列表   friendid  id   status
+    vector<User> query(int userid);
+};
+
+
+#endif
